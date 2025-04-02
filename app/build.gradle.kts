ec524c782c72e6984cc8b44716c471e8c443a302
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-02 21:56:57 +0300

    Add initial project files and resources for NearBarber app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.glyadgzl.nearbarber"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.glyadgzl.nearbarber"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -56,4 +56,17 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("io.coil-kt:coil-compose:2.2.2")
+implementation("com.google.accompanist:accompanist-pager-indicators:0.28.0")
+implementation("com.google.accompanist:accompanist-pager:0.28.0")
+implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
+implementation("com.github.bumptech.glide:glide:4.13.0")
+implementation("com.google.code.gson:gson:2.10.1")
+implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
+implementation("androidx.compose.foundation:foundation:1.7.8")
+implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
+implementation("com.google.maps.android:maps-compose:2.0")
+
 }
