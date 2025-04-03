e8c9180867d5f5d358494e33d20c0c7fed065e97
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 21:59:30 +0300

    Add loadPopular method to ResultsViewModel for fetching popular stores

--- app/src/main/java/com/glyadgzl/nearbarber/ViewModel/ResultsViewModel.kt
+++ app/src/main/java/com/glyadgzl/nearbarber/ViewModel/ResultsViewModel.kt
@@ -4,4 +4,8 @@ class ResultsViewModel {
     fun loadSubCategory(id: String): LiveData<MutableList<CategoryModel>> {
         return repository.loadSubCategory(id)
     }
+
+    fun loadPopular(id: String): LiveData<MutableList<StoreModel>> {
+        return repository.loadPopular(id)
+    }
  }
