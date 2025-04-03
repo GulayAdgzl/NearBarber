dcbc9cf0b1ed1d1cc8b64dc2019e8f27e8811106
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 21:13:36 +0300

    Add ResultsActivity and update dependencies for enhanced functionality

---
+++ app/src/main/java/com/glyadgzl/nearbarber/Results/ResultActivity.kt
@@ -0,0 +1,36 @@
+package com.glyadgzl.nearbarber.Results
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.glyadgzl.nearbarber.R
+
+class ResultsActivity : AppCompatActivity() {
+    private var id: String = ""
+    private var title: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        id=intent.getStringExtra(name="id")?:""
+        title=intent.getStringExtra(name="title")?:""
+
+        setContent {
+            ResultList(id,title,onBackClick={finish()})
+        }
+    }
+}
+
+@Composable
+fun ResultList(id: String="1", title: String="result", onBackClick: () -> Unit) {
+
+    LazyColumn (
+        modifier=Modifier.fillMaxSize().
+        background(color = colorResource(R.color.light_blue))
+    ){ 
+        item{TopTitle(title,onBackClick)}
+    }
+}
