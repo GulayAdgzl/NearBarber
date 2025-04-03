8e8acf880cf57106e886207cfa3224c9d35989b9
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-04 00:39:13 +0300

    Add MapActivity to handle map functionalities

---
+++ app/src/main/java/com/glyadgzl/nearbarber/Map/MapActivity.kt
@@ -0,0 +1,14 @@
+package com.glyadgzl.nearbarber.Map
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.glyadgzl.nearbarber.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
