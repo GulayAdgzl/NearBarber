dfebdf52522451c33f517f23a548761cd1499ad6
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 20:13:26 +0300

    Enhance Banner composable to support loading state and add Sliding composable for banner display

--- app/src/main/java/com/glyadgzl/nearbarber/Dashboard/Banner.kt
+++ app/src/main/java/com/glyadgzl/nearbarber/Dashboard/Banner.kt
@@ -1,4 +1,26 @@
 @Composable
-fun Banner(){
-    
+fun Banner(banners: SnapshotStateList<BannerModel>, showBannerLoading: Boolean) {
+    if(showBannerLoading){
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .height(200.dp),
+            contentAlignment = Alignment.Center
+        ){
+            CircularProgressIndicator()
+        }
+    }else{
+        
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun Sliding(
+    pagerState:PagerState= remember { PagerState() },
+    banners: List<BannerModel>
+){
+    HorizontalPager(count=banners.size,
+        state = pagerState
+    ) { 
+    }
 }
