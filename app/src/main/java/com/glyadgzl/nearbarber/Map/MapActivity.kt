6a32229e8280465da681c5513fd5376dd591b1b2
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-04 00:43:40 +0300

    Initialize StoreModel in MapActivity and enable edge-to-edge layout

--- app/src/main/java/com/glyadgzl/nearbarber/Map/MapActivity.kt
+++ app/src/main/java/com/glyadgzl/nearbarber/Map/MapActivity.kt
@@ -8,7 +8,17 @@ import androidx.core.view.WindowInsetsCompat
 import com.glyadgzl.nearbarber.R
 
 class MapActivity : AppCompatActivity() {
+    private lateinit var item: StoreModel
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        
+        setContent {
+            item = intent.getSerializableExtra("object") as StoreModel
+            
+            val latitude = item.latitude
+            val longitude = item.longitude
+        }
     }
 }
