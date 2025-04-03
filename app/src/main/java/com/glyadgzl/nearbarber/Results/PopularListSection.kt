07a4ce5311ba254c5914d4194853dc0006391b05
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 22:15:59 +0300

    Enhance PopularSection to display loading indicator and list of popular stores

--- app/src/main/java/com/glyadgzl/nearbarber/Results/PopularListSection.kt
+++ app/src/main/java/com/glyadgzl/nearbarber/Results/PopularListSection.kt
@@ -1,5 +1,5 @@
 @Composable
-fun PopularSection(popular:SnapshotStateList<StoreModel>, showPopularLoading: Boolean) {
+fun PopularSection(list:SnapshotStateList<StoreModel>, showPopularLoading: Boolean) {
     Row(
         Modifier
             .padding(horizontal = 16.dp)
@@ -19,4 +19,61 @@ fun PopularSection(popular:SnapshotStateList<StoreModel>, showPopularLoading: Bo
             style = TextStyle(textDecoration = TextDecoration.Underline)
         )
      }
-}
+     if(showPopularLoading){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+
+     }else{
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 8.dp)
+         ) {
+            items(list.size) { index ->
+                ItemsPopular(item = list[index])
+            }
+         }
+        }
+    }
+         
+         @Composable
+         fun ItemsPopular(item: StoreModel) {
+            val context = LocalContext.current
+            
+            Column (
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .wrapContentSize()
+                    .background(Color.White, shape = RoundedCornerShape(10.dp))
+                    .padding(8.dp)
+                    .clickable {
+
+                    }
+            ){
+                AsyncImage(
+   model = item.ImagePath,
+   contentDescription = null,
+   modifier = Modifier
+       .size(135.dp, 90.dp)
+       .clip(RoundedCornerShape(10.dp))
+       .background(colorResource(R.color.grey), shape = RoundedCornerShape(10.dp)),
+   contentScale = ContentScale.Crop
+)
+Text(
+   text=item.Title,
+   color = Color.Black,
+   fontSize = 14.sp,
+   fontWeight = FontWeight.Bold,
+   maxLines = 1,
+   overflow = TextOverflow.Ellipsis,
+   modifier = Modifier.padding(top=8.dp)
+)
+            }
+         }
