616e031594f1c67d89aa2fbba0a027df598460bc
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 13:24:24 +0300

    Refactor MainActivity: Replace Greeting composable with DashboardScreen and integrate MyBottomBar

--- app/src/main/java/com/glyadgzl/nearbarber/Dashboard/MainActivity.kt
+++ app/src/main/java/com/glyadgzl/nearbarber/Dashboard/MainActivity.kt
@@ -18,30 +18,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NearBarberTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+         DashboardScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NearBarberTheme {
-        Greeting("Android")
-    }
+@Preview
+fun DashboardScreen() {
+    
+        Scaffold(
+            
+            bottomBar = { MyBottomBar() }
+        ) { innerPadding ->
+           LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .backgroundColor(color=colorResource(R.color.lightBlue))
+                    .padding(paddingValues=innerPadding)
+            ) {
+                
+            }
+        }
+    
 }
