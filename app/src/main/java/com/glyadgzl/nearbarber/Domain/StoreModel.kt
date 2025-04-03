8e7b3c180fad90bf4dba724058db2fab110d60d2
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 21:54:50 +0300

    Add StoreModel data class for managing store information

---
+++ app/src/main/java/com/glyadgzl/nearbarber/Domain/StoreModel.kt
@@ -0,0 +1,13 @@
+data class StoreModel(
+    var Id:Int=0,
+    var CategoryId:String="",
+    var Title:String="",
+    var Latitude:Double=0.0,
+    var Longitude:Double=0.0,
+    var Call:String="",
+    var Address:String="",
+    var Activity:String="",
+    var ShortAddress:String="",
+    var Hours:String="",
+    var ImagePath:String=""
+):Serializable
