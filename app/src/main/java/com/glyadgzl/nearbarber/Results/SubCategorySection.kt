ddf5a19e412734aef6415dbd2a25e0fa9a4de995
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-03 21:46:46 +0300

    Add loading indicator and update SubCategorySection layout for better user experience

--- app/src/main/java/com/glyadgzl/nearbarber/Results/SubCategorySection.kt
+++ app/src/main/java/com/glyadgzl/nearbarber/Results/SubCategorySection.kt
@@ -3,5 +3,45 @@ fun SubCategorySection(
     subCategory: MutableList<CategoryModel>,
     showsubCategoryLoading: Boolean
 ) {
+    if(showsubCategoryLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpacedBy(12.dp),
+                contentPadding = PaddingValues
+                (start=16.dp,end=16.dp,top=8.dp)
+
+            
+        ){
+            items(subCategory.size) { index ->
+                Category(item=subCategory[index],
+                    onItemClick={}
+                )
+            }
+            
+            @Composable
+            fun Category(item: CategoryModel, onItemClick: () -> Unit) {
+                Column (modifier = Modifier
+                    .size(85.dp,95.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+                    .clickable (onClick = onItemClick),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    AsyncImage(model = item.ImagePath
+                        ,contentDescription = null
+                    )
+                }
+        }
+     
+    }
    
 }
