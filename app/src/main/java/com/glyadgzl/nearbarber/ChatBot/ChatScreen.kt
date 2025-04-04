656294ab7630c4f5acb41e586c4794194b10684b
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-04 16:08:17 +0300

    Add ChatScreen composable to manage chat UI and user interactions

---
+++ app/src/main/java/com/glyadgzl/nearbarber/ChatBot/ChatScreen.kt
@@ -0,0 +1,174 @@
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatScreen(paddingValues: PaddingValues) {
+    val chaViewModel = viewModel<ChatViewModel>()
+    val chatState = chaViewModel.chatState.collectAsState().value
+
+    val bitmap = getBitmap()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = paddingValues.calculateTopPadding()),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            reverseLayout = true
+        ) {
+            itemsIndexed(chatState.chatList) { index, chat ->
+                if (chat.isFromUser) {
+                    UserChatItem(
+                        prompt = chat.prompt, bitmap = chat.bitmap
+                    )
+                } else {
+                    ModelChatItem(response = chat.prompt)
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp, start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column {
+                bitmap?.let {
+                    Image(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(bottom = 2.dp)
+                            .clip(RoundedCornerShape(6.dp)),
+                        contentDescription = "picked image",
+                        contentScale = ContentScale.Crop,
+                        bitmap = it.asImageBitmap()
+                    )
+                }
+
+                Icon(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clickable {
+                            imagePicker.launch(
+                                PickVisualMediaRequest
+                                    .Builder()
+                                    .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                                    .build()
+                            )
+                        },
+                    imageVector = Icons.Rounded.AddPhotoAlternate,
+                    contentDescription = "Add Photo",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            TextField(
+                modifier = Modifier
+                    .weight(1f),
+                value = chatState.prompt,
+                onValueChange = {
+                    chaViewModel.onEvent(ChatUiEvent.UpdatePrompt(it))
+                },
+                placeholder = {
+                    Text(text = "Type a prompt")
+                }
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Icon(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clickable {
+                        chaViewModel.onEvent(ChatUiEvent.SendPrompt(chatState.prompt, bitmap))
+                        uriState.update { "" }
+                    },
+                imageVector = Icons.Rounded.Send,
+                contentDescription = "Send prompt",
+                tint = MaterialTheme.colorScheme.primary
+            )
+
+        }
+
+    }
+
+}
+
+@Composable
+fun UserChatItem(prompt: String, bitmap: Bitmap?) {
+    Column(
+        modifier = Modifier.padding(start = 100.dp, bottom = 16.dp)
+    ) {
+
+        bitmap?.let {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(260.dp)
+                    .padding(bottom = 2.dp)
+                    .clip(RoundedCornerShape(12.dp)),
+                contentDescription = "image",
+                contentScale = ContentScale.Crop,
+                bitmap = it.asImageBitmap()
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp),
+            text = prompt,
+            fontSize = 17.sp,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+
+    }
+}
+
+@Composable
+fun ModelChatItem(response: String) {
+    Column(
+        modifier = Modifier.padding(end = 100.dp, bottom = 16.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+                .background(Green)
+                .padding(16.dp),
+            text = response,
+            fontSize = 17.sp,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+
+    }
+}
+
+@Composable
+private fun getBitmap(): Bitmap? {
+    val uri = uriState.collectAsState().value
+
+    val imageState: AsyncImagePainter.State = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(uri)
+            .size(Size.ORIGINAL)
+            .build()
+    ).state
+
+    if (imageState is AsyncImagePainter.State.Success) {
+        return imageState.result.drawable.toBitmap()
+    }
+
+    return null
+}
+}
