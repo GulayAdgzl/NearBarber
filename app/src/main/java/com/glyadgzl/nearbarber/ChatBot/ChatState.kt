bafa0acec8600669b4694ea64593496387f6753a
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-04 16:05:13 +0300

    Add ChatState data class to manage chat list, prompt, and bitmap

---
+++ app/src/main/java/com/glyadgzl/nearbarber/ChatBot/ChatState.kt
@@ -0,0 +1,9 @@
+import android.graphics.Bitmap
+import com.ahmedapps.geminichatbot.data.Chat
+
+
+data class ChatState (
+    val chatList: MutableList<Chat> = mutableListOf(),
+    val prompt: String = "",
+    val bitmap: Bitmap? = null
+)
